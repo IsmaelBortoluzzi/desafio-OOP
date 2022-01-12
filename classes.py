c820250ac81ed0df11510f673c741ac72c5bcd08
IsmaelBortoluzzi <ismael.bortoluzzi77@gmail.com>
2022-01-12 14:52:32 -0300

    Preparando as classes

---
+++ classes.py
@@ -0,0 +1,25 @@
+class Banco:
+    pass
+
+
+class Pessoa:
+    pass
+
+
+class Cliente:
+    pass
+
+
+class Conta:
+    pass
+
+
+class ContaCorrente(Conta):
+    pass
+
+
+class ContaPoupanca(Conta):
+    pass
+
+
+class
